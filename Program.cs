37107ca2f6f9e5f7fd32763036a1890b911500bb
joshu <joshu@REEDPC>
2024-07-08 16:05:35 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Assignment10._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //The Three Parts of a LINQ Query:
+            //1. Data source
+            int[] numbers = [1, 4, 0, -5, 9, -11];
+
+            //2. Query creation.
+            //numQuery is an IEnumerable<int>
+            var numQuery = from num in numbers
+                           where (num % 2) >= 0
+                           select num;
+
+            //3. Query execution
+            foreach (int num in numQuery)
+            {
+                Console.Write("{0, 1} " , num);
+            }
+        }
+    }
+}
